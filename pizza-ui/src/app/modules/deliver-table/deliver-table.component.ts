cbdfe8efce0c23b2d6c305a2e085060183726bba
Neldrick <neldrick01@gmail.com--global>
2022-08-20 16:12:28 +0800

    feat: TBSP-0004-str ui structure

---
+++ pizza-ui/src/app/modules/deliver-table/deliver-table.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-deliver-table',
+  templateUrl: './deliver-table.component.html',
+  styleUrls: ['./deliver-table.component.scss']
+})
+export class DeliverTableComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ pizza-ui/src/app/modules/pizza-order/pizza-selection/pizza-selection.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pizza-selection',
+  templateUrl: './pizza-selection.component.html',
+  styleUrls: ['./pizza-selection.component.scss']
+})
+export class PizzaSelectionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
